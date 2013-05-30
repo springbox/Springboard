deb871903f0285e8d5c1d869e5cfe7e6cacab9c9
Josh Matz <josh.matz@gmail.com>
2013-05-30 12:01:56 -0500

    No necesito.

--- library/ext/Singularity-custom/lib/singularitygs.rb
+++
@@ -1,9 +0,0 @@
-require 'compass'
-require 'breakpoint'
-
-Compass::Frameworks.register("singularitygs", :path => "#{File.dirname(__FILE__)}/..")
-
-module SingularityGS
-  VERSION = "1.0.8"
-  DATE = "2013-04-25"
-end
--- library/ext/respond-to-custom/compass/lib/respond-to.rb
+++
@@ -1,5 +0,0 @@
-require 'compass'
-require 'breakpoint'
-
-extension_path = File.expand_path(File.join(File.dirname(__FILE__), ".."))
-Compass::Frameworks.register('respond-to', :path => extension_path)
