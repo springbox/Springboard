15c12d0b6fde31730138ad3388e0979721bd62fc
Josh Matz <josh.matz@gmail.com>
2013-06-06 17:54:48 -0500

    Off canvas, yo!

--- library/js/main.js
+++ library/js/main.js
@@ -1,2 +1,9 @@
 
 // http://alistapart.com/article/the-design-of-code-organizing-javascript
+
+;$(function() {
+	$('#js-offcanvas-button').on('click', function(e) {
+		e.preventDefault();
+		$('.sb-offcanvas-duo-inner').toggleClass('sb-offcanvas-duo-active');
+	});
+});
